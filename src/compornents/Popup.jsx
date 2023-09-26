bc3ae448b042a9a7c9b20e11da6b9bfb401c9504
yoochan0108 <yoochan0108@naver.com>
2023-09-26 09:48:47 +0900

    컴퍼넌트 생성

--- src/compornents/Popup.jsx
+++ src/compornents/Popup.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 function Popup() {
-	return <aside>Popup</aside>;
+	const [Num, setNum] = useState(0);
+	useEffect(() => {
+		//컴포넌트 mount시 한번만 실행
+		console.log('Popup생성');
+
+		return () => {
+			//컴포넌트가 unmount시 한번만 실행
+			console.log('Popup소멸');
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log('popup');
+	}, []);
+
+	return (
+		<aside>
+			<h1>{Num}</h1>
+			<button onClick={() => setNum(Num - 1)}>minus</button>
+			<button onClick={() => setNum(Num + 1)}>plus</button>
+		</aside>
+	);
 }
 
 export default Popup;
